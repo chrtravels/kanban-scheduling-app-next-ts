71b830edf0a03277aff89f3e86a27e959f7265b7
Chris Reynoso <chrtravels@gmail.com>
2023-11-14 12:46:41 +0700

    dark mode toggle working

---
+++ components/navbar/Navbar.jsx
@@ -0,0 +1,70 @@
+'use client'
+
+import { useTheme } from 'next-themes'
+
+import { useEffect, useState } from 'react';
+import Image from 'next/image';
+import styles from './navbar.module.scss'
+
+
+const Navbar = () => {
+  const { theme, setTheme} = useTheme();
+  const [mounted, setMounted] = useState(false)
+
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  console.log('resolved theme: ', theme)
+  if (!mounted) {
+    return null
+  }
+
+  console.log('theme: ', useTheme())
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.leftContent}>
+        <div className={styles.logo}>
+          {theme === 'dark' ? (
+            <Image
+            src='/assets/logo-light.svg'
+            height={26}
+            width={153}
+            alt='kanban logo'
+            />
+           ) :
+            (
+            <Image
+            src='/assets/logo-dark.svg'
+            height={26}
+            width={153}
+            alt='kanban logo'
+            />
+            )
+          }
+        </div>
+        <div className={styles.boardName}></div>
+      </div>
+
+      <div className={styles.navbarActions}>
+        <div className={styles.addTaskBtn}>
+        <button
+          aria-label='Toggle Dark Mode'
+          type='button'
+          onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')}
+          >
+          {theme === 'dark' ? (
+            'light'
+          ) : (
+            'dark'
+          )}
+        </button>
+      </div>
+    </div>
+    </div>
+  )
+}
+
+export default Navbar;
