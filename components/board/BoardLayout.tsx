2f4b704b89d39c919524f60aca37ff16c4e1d44e
Chris Reynoso <chrtravels@gmail.com>
2023-11-18 12:43:59 +0700

    made board display conditional on having content

--- components/board/BoardLayout.tsx
+++ components/board/BoardLayout.tsx
@@ -8,6 +8,7 @@ import TaskCards from './boardCard/TaskCards';
 export default function BoardLayout (props: { boards: {[key: string]: any[]}, boardName: string}) {
   const currentBoardName = props.boardName.includes('-') ? props.boardName.split('-').join(' ') : props.boardName;
   const selectedBoard = props.boards[currentBoardName]
+  console.log(selectedBoard)
 
   const bulletColorArray = [
     '#49c4e5',
@@ -18,30 +19,31 @@ export default function BoardLayout (props: { boards: {[key: string]: any[]}, bo
   return (
     <div className={styles.container}>
       {selectedBoard.map((column, idx) => {
-
-        return (
-          <div key={idx} className={styles.column}>
-            <div className={styles.columnHeader}>
-              {idx <= bulletColorArray.length - 1 &&
-                <div className={styles.bullet} style={{backgroundColor: bulletColorArray[idx]}}></div>
-              }
-              <div className={styles.title}>{column.status.toUpperCase()} <span>({column.tasks.length})</span></div>
-            </div>
-
-            <div className={styles.columnBody}>
-              {selectedBoard.map((board, idx) => {
-
-                if (column.status === board.status) {
-                  const tasks: [{title: string, status: string, subtasks: [], description: string}] = board.tasks;
-
-                  return (
-                    <TaskCards tasks={tasks} />
-                  )
+        if (column.tasks.length > 0) {
+          return (
+            <div key={column.status} className={styles.column}>
+              <div className={styles.columnHeader}>
+                {idx <= bulletColorArray.length - 1 &&
+                  <div className={styles.bullet} style={{backgroundColor: bulletColorArray[idx]}}></div>
                 }
-              })}
+                <div className={styles.title}>{column.status.toUpperCase()} <span>({column.tasks.length})</span></div>
+              </div>
+
+              <div className={styles.columnBody}>
+                {selectedBoard.map((board, idx) => {
+
+                  if (column.status === board.status) {
+                    const tasks: [{title: string, status: string, subtasks: [], description: string}] = board.tasks;
+
+                    return (
+                      <TaskCards tasks={tasks} />
+                    )
+                  }
+                })}
+              </div>
             </div>
-          </div>
-        )
+          )
+        } else return;
       })}
 
     </div>
