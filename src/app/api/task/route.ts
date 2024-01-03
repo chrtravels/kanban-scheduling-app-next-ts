51d66b1ffdf8b0b7b0744263a1a9168bba10df80
Chris Reynoso <chrtravels@gmail.com>
2024-01-03 12:27:21 -0800

    removed NextRequest from task route

--- src/app/api/task/route.ts
+++ src/app/api/task/route.ts
@@ -1,4 +1,4 @@
-import { NextRequest, NextResponse } from "next/server";
+import { NextResponse } from "next/server";
 import conn from '../../../../lib/db';
 
 export async function GET(request: Request) {
